0450bb9fff277b88ad848074aa93d78009f6dd77
Jason <jasonchrisdudu@gmail.com>
2019-02-16 16:08:14 +0800

    [190216: finish create-topic-fun]

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,8 +5,7 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = [
-        'title', 'body', 'user_id', 'category_id', 'reply_count', 
-        'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'
+        'title', 'body', 'category_id', 'excerpt', 'slug'        
     ];
     //category —— 一个话题属于一个分类
     public function category()
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -44,7 +44,7 @@ class UsersTableSeeder extends Seeder
 
             // 单独处理第一个用户的数据
             $user = User::find(1);
-            $user->name = 'Jason';
+            $user->name = 'Jason';            
             $user->email = 'jasonchrisdudu@gmail.com';
             $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png';
             $user->save();
